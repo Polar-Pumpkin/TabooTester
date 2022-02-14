dfac4d93cbe9583077083beaf5b581305293abd7
Legoshi <entityparrot@qq.com>
2022-02-14 19:18:57 +0800

    [+] 新增对 EntityDeathEvent 的调试信息
    本次 Commit 共包含以下内容:
    - [+] ...

--- src/main/kotlin/org/serverct/parrot/tabootester/event/EntityDeathListener.kt
+++ src/main/kotlin/org/serverct/parrot/tabootester/event/EntityDeathListener.kt
@@ -5,11 +5,20 @@ import org.bukkit.entity.EntityType
 import org.bukkit.event.entity.EntityDeathEvent
 import org.bukkit.inventory.ItemStack
 import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common.platform.function.info
 
 object EntityDeathListener {
 
     @SubscribeEvent
     fun onDeath(event: EntityDeathEvent) {
+        """
+            EntityDeathEvent {
+                Entity: ${event.entityType}
+                Experience: ${event.droppedExp}
+                Drops: ${event.drops.size}
+            }
+        """.trimIndent().split('\n').forEach { info(it) }
+
         when (event.entity.type) {
             EntityType.ZOMBIE -> {
                 event.drops.clear()
